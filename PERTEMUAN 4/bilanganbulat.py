2dca78086c7121bbe7bcb1adaa2b76a016cca813
JossRizalS <142191903+JossRizalS@users.noreply.github.com>
2024-02-05 09:21:29 +0700

    Add files via upload

---
+++ PERTEMUAN 4/bilanganbulat.py
@@ -0,0 +1,10 @@
+x = 78
+
+if (x > 0):
+    print ("bilangan tersebut adalah bilangan positif")
+elif (x > 0):
+    print ("bilangan tersebut adalah bilangan negatif")
+else:
+    print ("bilangan tersebut adalah 0")
+
+    
---
+++ PERTEMUAN 4/bilanganbulat2.py
@@ -0,0 +1,9 @@
+x = 3
+y = 2
+
+if (x > y):
+    print ("bilangan tersebut adalah bilangan kedua")
+elif (x < y):
+    print ("bilangan tersebut adalah bilangan pertama")
+else:
+    print ("bilangan tersebut adalah sama")
